781e7d2c8353e4fa6216cf67ab138495f4e5a142
Manoochehrbahramkhan <126709446+Manoochehrbahramkhan@users.noreply.github.com>
2024-11-05 21:33:11 +0330

    Create color.sh

---
+++ color.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# most popular color in linux!
+red="\033[31m"
+green="\033[32m"
+yellow="\033[33m"
+blue="\033[34m"
+Purple="\033[35m"
+white="\033[37m"
+black="\033[30m"
+reset="\033[0m"
+
+echo -e "${green} hello world! ${reset}"
