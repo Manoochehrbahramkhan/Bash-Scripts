49a3d6e0abb42df6a013cfc3d353e984a1bdeea2
Manoochehrbahramkhan <126709446+Manoochehrbahramkhan@users.noreply.github.com>
2024-11-09 11:49:04 +0330

    Create farsi-bbb.sh

---
+++ farsi-bbb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+green="\033[32m"
+reset="\033[0m"
+#pannel farsi
+cd /root/greenlight
+docker-compose down
+
+wait
+
+cd /root/greenlight-v3
+docker exec -it greenlight-v3 bundle exec rake admin:create["admin","email@example.com","user","password"]
+
+sed -i '81iDEFAULT_LOCALE=fa_IR' /root/greenlight-v3/.env
+sed -i 's/overrideLocale: null/overrideLocale: fa/g' /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
+docker-compose down
+docker-compose up -d
+wait
+
+cp /home/iauec/default.pdf /var/www/bigbluebutton-default/assets
+sleep 2
+
+if [[ $? == 0 ]];then
+        echo -e "${green} your system will be reboot in 3s ${reset}"
+        sleep 3
+        reboot
+fi
