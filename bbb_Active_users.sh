0c95c098b5b1935773c0ddab6b6dde2a2b1e35a3
Manoochehrbahramkhan <126709446+Manoochehrbahramkhan@users.noreply.github.com>
2025-01-26 08:27:30 +0330

    Update bbb_Active_users.sh

--- bbb_Active_users.sh
+++ bbb_Active_users.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-API_URL="https://your_domain_name/bigbluebutton/api/"
-SECRET="your_secret_key"
+API_URL="https://your-bbb-server.com/bigbluebutton/api/"
+SECRET="your_shared_secret"
 
+#Active Sessions
 response=$(curl -s "${API_URL}getMeetings?checksum=$(echo -n "getMeetings${SECRET}" | sha1sum | cut -d' ' -f1)")
 
-if echo "$response" | jq empty > /dev/null 2>&1; then
+if echo "$response" | xmlstarlet fo > /dev/null 2>&1; then
+    meetings_count=$(echo "$response" | xmlstarlet sel -t -v "count(//meetings/meeting)" 2>/dev/null)
 
-    total_users=$(echo "$response" | jq '[.meetings[].participantCount] | add')
-
-    if [[ -z "$total_users" || "$total_users" == "null" ]]; then
-        total_users=0
+    if [[ "$meetings_count" -eq 0 ]]; then
+        echo "Not Active Sessions!."
+    else
+        
+        total_users=$(echo "$response" | xmlstarlet sel -t -v "sum(//meetings/meeting/participantCount)" 2>/dev/null)
+        echo "Active Sessions: $meetings_count"
+        echo "Acticve Users: $total_users"
     fi
-
-    echo "Active Users: $total_users"
 else
-    echo "ERROR Server Response Not Valid."
-    echo "Server Response:"
+    echo "Error Server Not Response."
+    echo "Response Server:"
     echo "$response"
 fi
